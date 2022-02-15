21fbd018b605cabd283745c9becd22c66f3305a3
Tushant Mehta(300186940) <Tushant.Mehta@student.ufv.ca>
2022-02-14 23:34:29 -0800

    added all initial files

---
+++ index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" type="text/css" href="design.css">
+
+    </head>
+    <body>
+       
+        <div class="home">
+        <!--Navbar  -->
+        <div class="navbar">
+            <ul>
+                <li>
+                    <a href="#"><img src="https://icon-library.com/images/website-homepage-icon/website-homepage-icon-17.jpg" alt=""></a>
+                </li>
+                <li><a href="#education">Studies</a></li><!-- comment -->
+                <li><a href="#skills">Personal Skills</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#experience">Experience</a></li>
+                
+            </ul>
+        </div>
+        <div class="right">
+            <div class="detail">
+                <header>
+                    <h2>Tushant Mehta</h2>
+                    <h3>7724 142 street</h3>
+                    <h3>Surrey, BC</h3>
+                    <h3>778-798-7747</h3>
+                    <h3>tushantmehta@yahoo.com</h3>
+                    <hr>
+                    <h4>Summary</h4>
+                    <p>Analytical and process-oriented with Computer Information System diploma, Strives toward continuous improvement
+                        through progressive practices and critical thinking. Open-minded and a quick learner, able to pick up on new processes,
+                        systems, and technologies quickly. Team player who thrives while working collaboratively or independently. Excellent
+                        time management and multitasking skills. Skill in programing, hardware and networking.
+                    </p>
+
+                </header>
+            </div>
+            
+            <br><hr>
+            <!-- Academic -->
+            <div id="education">
+                <h3><a href="https://www.ufv.ca/cis/programs/cis-diploma/">Computer Information System Diploma</a></h3>
+                <p>University of the Fraser Valley • Abbotsford, British Columbia</p>
+                <ul>
+                    <li>Good in studies: 3.7+ GPA</li>
+                    <li>Been on Dean's scholar list of distinguished students</li>
+                </ul>
+                
+            </div>
+            
+            <br><hr>
+            
+            <!-- Skills -->
+            <div id="skills">
+                <h3>Skills:</h3>
+                <div class="left-skills">
+                    <ul>
+                        <li>Python</li>
+                        <li>C++</li>
+                        <li>Web Development</li>
+                        <li>Linux</li>
+                        <li>Networking</li>
+                        <li>Business Intelligence</li>
+                    </ul>
+                    
+                </div>
+                <div class="right-skills">
+                    <ul>
+                        <li>Database</li>
+                        <li>Customer Service</li>
+                        <li>Git</li>
+                        <li>Microsoft Office</li>
+                        <li>Organizational Skills</li>
+                    </ul>
+                </div>
+            </div>
+            
+            <br><hr>
+            
+            <!-- Projects -->
+            <div id="projects">
+                <h3>Project:</h3>
+                <ul>
+                    <li>Website development (Javascript, php, SQL)</li>
+                    <li>TicTacToe GUI game using python.</li><!-- comment -->
+                    <li>Canon shot simulator using python.</li>
+                </ul>
+                <br><!-- comment -->
+                <p><a href="https://github.com/tmm78795">Click here</a> to see projects on github</p>
+            </div>
+            
+            <br><hr>
+            
+            <!--Experiences -->
+            <div id="experience">
+                <h3> Experience</h3>
+                <ul>
+                    <li><h4>Sales Associate</h4></li><!-- comment -->
+                            
+                </ul>
+            </div>
+            
+       
+        </div> <!-- right main div end -->
+      
+    </div> <!-- main div end -->
+    </body>
+</html>
